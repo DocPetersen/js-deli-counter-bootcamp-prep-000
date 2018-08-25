09c697a7e8e803068fd505bf8152b724205672fa
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-08-25 06:24:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return "Welcome, " . You are number 1 in line.";
+  return 'Welcome, ${name}. You are number 1 in line.";
 }
 
 function nowServing(katzDeliLine) {
