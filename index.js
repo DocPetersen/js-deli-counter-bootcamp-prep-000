f5a83953c612bdd8a842f8e2d9584d5f233e0deb
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-08-25 06:56:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing (katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  if (katzDeliLine === 0);
+  if (katzDeliLine === 0) {
   return "The line is currently empty.";
 }
   else {
