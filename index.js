3726abfee1811635a1f2d75a3214b7eb66fc2343
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-08-25 06:36:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing (katzDeliLine) {
   var next = katzDeliLine.shift();
-  return next ? `Currently serving number ${katzDeliLine.length};
+  return next ? `Currently serving number ${katzDeliLine.length}` : 
 }
